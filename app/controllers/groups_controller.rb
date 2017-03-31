dd72446013472a8820f847621b41f19732455caf
puretor <mickie0306@gmail.com>
2017-03-31 20:35:01 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
-
+  end
 
   def show
      @group = Group.find(params[:id])
@@ -17,22 +17,24 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+     @group = Group.new(group_params)
 
     if @group.save
         redirect_to groups_path
     else
-        ender :new
+        render :new
     end
-   end
+  end
 
       def update
         @group = Group.find(params[:id])
-
-        @group.update(group_params)
-
-        redirect_to groups_path, notice: "Update Success"
+        if @group.update(group_params)
+          redirect_to groups_path, notice: "Update Success"
+        else
+          render :edit
+        end
       end
+
         def destroy
           @group = Group.find(params[:id])
           @group.destroy
@@ -40,8 +42,9 @@ class GroupsController < ApplicationController
           redirect_to groups_path
         end
 
-       private
+    private
 
     def group_params
       params.require(:group).permit(:title, :description)
     end
+  end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,3 @@
 class Group < ApplicationRecord
-  class Group < ApplicationRecord
-   validates :title, presence: true
+  validates :title, presence: true
 end
